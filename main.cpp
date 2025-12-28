168ec57d8e7310d779f9c83057e73068c407b25e
SophiaRaytchinova <sraychinova@gmail.com>
2025-12-28 18:54:33 +0200

    edited userExists function and added registerUser function

--- main.cpp
+++ main.cpp
@@ -62,20 +62,63 @@ void printHand(const std::vector<std::string>& hand) {
 
 // --Functions for profiles--
 //userExists function checks if the user trying to log exists in the files or not
-bool userExists(const std::string& usernames, const std::string& passwords) {
-    std::ifstream file("user.txt"); //opens file
-    if (!file.is_open()) { //checks if file is open
-        cout << "Error opening file!" << endl;
+bool userExists(const std::string& logUsername) {
+    if (logUsername.empty()) {
+        return false;
+    }
+    std::ifstream user("users.txt"); //opens file in read mode
+    if (!user.is_open()) { //checks if file is open
         return false;
     }
     std::string username, password;
-    while (file >> username >> password) { //reads the file line by line
-        if (username == usernames && password == passwords) {
-            file.close();
+    
+    while (user >> username >> password) { //reads the file with users and passwords line by line
+        if (username == logUsername) {
+            user.close();
             return true; // user found
         }
-        else return false; //user not found
     }
+    user.close();
+    return false; //user not found
+}
+
+bool registerUser(const std::string& newUsername, const std::string& newPassword) {
+    if (newUsername.empty() || newPassword.empty()) {
+        cout << "Username and password cannot be empty! Enter valid username and password. " << endl;
+        return false;
+    }
+    if (userExists(newUsername)) {
+        cout << "User already exists!" << endl;
+        return false;
+    }
+    if (newUsername.find(' ') != std::string::npos) {
+        cout << "Username cannot contain spaces!" << endl;
+        return false;
+    }
+    if (newPassword.find(' ') != std::string::npos) {
+        cout << "Password cannot contain spaces!" << endl;
+        return false;
+    }
+    //append new user to user.txt
+    std::ofstream users("users.txt", std::ios::app); //opens file in append mode, so we can add content without removing its current content
+    if (!users.is_open()) { //checks if file is open
+        cout << "Error opening user.txt file!" << endl;
+        return false;
+    }
+    users << newUsername << " " << newPassword << endl; //writes username and password to file
+    users.close();
+    //createс a profile file for the new user
+    std::ofstream profile(newUsername + ".txt", std::ios::app); //creates a new file for user statistics
+    if (!profile.is_open()) {
+        cout << "Error creating profile file!" << endl;
+        return false;
+    }
+    profile << "Username: " << newUsername << endl;
+    profile << "Total games played: 0" << endl;
+    profile << "Total games won: 0 (0%)" << endl;
+    profile << "Games against other players (wins/%): " << endl;
+    profile.close();
+    return true;
 }
 
 int main() {
